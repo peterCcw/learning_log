e0bd128e69cb4ee6e438e1bf5e1f11d3ce74e3f1
peterCcw <piotr.ciecwierz@gmail.com>
2020-09-03 02:29:18 +0200

    Added registration page

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
-"""Defines templates of URL for learning_logs"""
+"""Defines templates of URL for users"""
 from django.urls import path, include
+
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # Adding default url authorization addresses
     path('', include('django.contrib.auth.urls')),
+    # Registration page
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    """Registration of new user"""
+    if request.method != 'POST':
+        # Displaying empty user registration form
+        form = UserCreationForm()
+    else:
+        # Processing filled form
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # Logging new user and redirecting him to main page
+            login(request, new_user)
+            return redirect('learning_logs:index')
+    # Displaying empty form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
